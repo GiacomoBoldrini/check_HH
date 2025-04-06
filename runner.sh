69aca6713356326a2cce287d92d20443829f5c8a
Giacomo Boldrini <gboldrin@cern.ch>
2025-04-06 16:13:00 +0200

    first commit

--- runner.sh
+++ runner.sh
@@ -8,20 +8,10 @@ echo ">> Setting RUN_DIR to ${RUN_DIR}"
 CMSSW_RELEASE=CMSSW_12_4_16
 SCRAM_ARCH=el8_amd64_gcc10
 
-if [ "${CMSSW_RELEASE}" != "local" ]; then
-    if [ -d ${CMSSW_RELEASE} ]; then
-      echo ">> Cleaning up existing ${CMSSW_RELEASE} directory"
-      rm -r ${CMSSW_RELEASE}
-    fi
-    echo ">> Setting up release area for ${CMSSW_RELEASE} and arch ${SCRAM_ARCH}"
-    if [ ! -d ${CMSSW_RELEASE} ]; then
-      scram project CMSSW ${CMSSW_RELEASE}
-    fi
+export SCRAM_ARCH=$SCRAM_ARCH
 
-    cd ${CMSSW_RELEASE}/src
-    eval `scramv1 runtime -sh`
-    cd -
+cmsrel $CMSSW_RELEASE
 
-fi
+cd $CMSSW_RELEASE/src; cmsenv; cd -
 
 cmsRun nanGenScratch.py
